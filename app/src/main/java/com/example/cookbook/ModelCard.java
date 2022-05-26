7b500801d4f0a710e9f88292b9c01c2ec5821ae5
Divya10112001 <divyapoonam12@gmail.com>
2022-05-26 18:08:08 +0530

    An app that serves your favourite dishes and desserts all just with one click.

---
+++ app/src/main/java/com/example/cookbook/ModelCard.java
@@ -0,0 +1,14 @@
+package com.example.cookbook;
+
+public class ModelCard {
+    private String url;
+
+    public ModelCard(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+}
